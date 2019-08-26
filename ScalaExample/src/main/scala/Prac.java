9582fc59d781aa02ca3c6536dfa70e9f36e21a2b
pavan <pavanreddy6117@gmail.com>
2019-08-26 10:35:30 +0530

    Initial commit

---
+++ ScalaExample/src/main/scala/Prac.java
@@ -0,0 +1,14 @@
+
+
+public class Prac {
+
+    public static void main(String[] args) {
+        System.out.println("main method");
+        Prac b = new Prac();
+        b.m(10);
+    }
+        void m ( int a){
+            System.out.println(" non static method");
+        }
+    }
+
